61a7231a97726bba0e1176c1b91c835d6a5433f2
sfSanfrod <985758828@qq.com>
2018-03-05 22:51:44 +0800

    numbers

--- 第二期/上海-小白龙/第二次任务/Day1.py
+++ 第二期/上海-小白龙/第二次任务/Day1.py
@@ -1,3 +1,38 @@
 #!/usr/bin/python
 #encoding=utf-8
+import math
+import random
+import cmath
 
+__author__=u'小白龙'
+class Numbers:
+    def mathFuc(self):
+        print(math.sqrt(9))#求平方
+        print(math.fabs(-5))
+        print(math.pi)#pai
+        print(math.log10(100))
+    def randomFuc(self):
+        print(random.randint(1,10))#返回int数值
+        print(random.random())#返回0-1之间的float型数值
+        print(random.choice([u"小白龙",u"慧慧",u"五娃"]))#Return a random element from the non-empty sequence seq
+    def cmathFuc(self):
+        print(cmath.cos(45))
+        print(cmath.log10(100))
+        print(cmath.e)
+class Strings:
+    pass
+if __name__=="__main__":
+    x = 12.83
+    y = 3
+    print(x)
+    print(y)
+    print(int(x))
+    print(float(y))
+    print(complex(x,y))
+    print("==========math==========")
+    num = Numbers()
+    num.mathFuc()
+    print("==========random==========")
+    num.randomFuc()
+    print("==========cmath==========")
+    num.cmathFuc()
