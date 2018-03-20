c16b42ee3e22e9570812321ce8e2aa72ad57b148
wuwaer <784785282@qq.com>
2018-03-20 18:02:37 +0800

    add:pytthon3_mysql_method

--- 第一期/北京-五娃/Practice/pymysqlOpreation.py
+++ 第一期/北京-五娃/Practice/pymysqlOpreation.py
@@ -53,19 +53,78 @@ class pymysqlopreations:
         except:
             result = {'code': '9999', 'message': u'执行单条查询异常', 'data': []}
             mylogger.info(u'Mysql查询失败！')
-        finally:
-            return result
-    def select_all(self):
-        pass
-    def opt(self):
-        pass
-    def close_con(self):
-        pass
+        return result
+        
+    def select_all(self, params):
+        """
+        查询所有内容
+        :param params:
+        :return:
+        """
+        try:
+            row = self.cur.execute(params)
+            if row > 0:
+                self.cur.scroll(0, mode='absolute')
+                results = self.cur.fetchall()
+                result = {'code': '0000', 'message': '执行查询所有数据成功', 'data': results}
+            else:
+                result = {'code': '0000', 'message': '执行查询所有数据成功', 'data': []}
+        except:
+            result = {'code': '9999', 'message': '执行查询所有数据异常', 'data': []}
+            mylogger.info('Mysql查询多条数据失败！')
+        return result
+
+    def opt_one(self, params):
+        """
+        操作单条数据
+        :param params:
+        :return:
+        """
+        try:
+            row = self.cur.execute(params)
+            self.conn.commit()
+            result = {'code': '0000', 'message': '执行单条数据成功', 'data': int(row)}
+        except:
+            result = {'code': '9999', 'message': '执行单条数据失败', 'data': []}
+            mylogger.info('Mysql操作单条数据失败')
+        return result
+
+    def opt_many(self, stmt, data):
+        """
+        多条数据
+        :param stmt:
+        :param data:
+        :return:
+        """
+        try:
+            row = self.cur.executemany(stmt, data)
+            self.conn.commit()
+            result = {'code': '0000', 'message': '插入多条数据成功', 'data': int(row)}
+        except:
+            result = {'code': '9999', 'message': '插入多条数据失败', 'data': []}
+            mylogger.info('Mysql 插入多条数据失败')
+        return result
+
+    def __del__(self):
+        if self.cur != None:
+            self.cur.close()
+        if self.conn != None:
+            self.conn.close()
 
 
 if __name__ == "__main__":
     conn = pymysqlopreations(host='127.0.0.1', port=3306, user='root', password='root', db='irootdb',
                              link_type=1)
-    sql = " SELECT * FROM `interface_detail`"
+       sql = " SELECT * FROM `interface_detail`"
     resutl = conn.select_one(sql)
     print(resutl)
+    sql_demo = """ INSERT INTO `interface_detail`(NAME,detail,param,check_value,exe_env,LEVEL,require_login) VALUES('xx','xx','xx','id=10',0,0,0)"""
+    roe = conn.opt_one(sql_demo)
+    print(roe)
+    results = conn.select_all(sql)
+    print(results)
+    updatestmt = ''' update `interface_detail` set LEVEL = %s where interface_id =%s'''
+    data = [(5,18)]
+    conn.opt_many(updatestmt,data)
+    results = conn.select_all(sql)
+    print(results)
