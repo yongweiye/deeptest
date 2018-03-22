b99957aff5937e15fbd21c27d8ca6697a665360f
gonghuihui55 <yulu@dianjia.io>
2018-03-22 12:57:03 +0800

    modified parse_xpath.py

--- 第二期/杭州-巩慧慧/第一期第三次任务/parse_xpath.py
+++ 第二期/杭州-巩慧慧/第一期第三次任务/parse_xpath.py
@@ -4,11 +4,13 @@
 # @Author  : gonghuihui
 # @File    : parse_xpath.py
 
-
-# 支持xpath提取或设置指定节点值
-# 支持遍历指定节点
-# 支持读取或设置指定节点的属性
 import xml.etree.ElementTree as ET
+
+# xpath提取或设置指定节点值
+# 遍历指定节点
+# 读取或设置指定节点的属性
+# 新增节点
+# 删除节点
 
 class ParseXpath:
     # 提取节点值
@@ -35,6 +37,19 @@ class ParseXpath:
         for child in root.iter(node):
             child.attrib = attrib
             # return 1
+
+    # 新增节点
+    def add_childnode(self, node, childnode, childnodetext):
+        for child in root.iter(node):
+            add_childnode = ET.SubElement(child, childnode)
+            add_childnode.text = childnodetext
+            # print("wx",add_childnode.text)
+
+    # 删除节点，有问题
+    def remove_childnode(self, childnode):
+        for child in root.findall(childnode):
+            print("删除节点", child)
+            root.remove(child)
 
 
 if __name__ == "__main__":
@@ -85,20 +100,26 @@ if __name__ == "__main__":
     update_year_text = parse_xpath.set_node_text("year","2017")
 
     # 给country新增一个<wx>开元优测</WX>
-    for child in root.iter("country"):
-        wx = ET.SubElement(child, "wx")
-        wx.text = "开源优测"
+    # for child in root.iter("country"):
+    #     wx = ET.SubElement(child, "wx")
+    #     wx.text = "开源优测"
+    parse_xpath.add_childnode("country", "wx", "开源优测")
+
 
     # 遍历wx节点，并打印
-    for child in root.iter("wx"):
-        print(child.tag, ":", child.text)
+    # for child in root.iter("wx"):
+    #     print(child.tag, ":", child.text)
+    parse_xpath.get_node_text("wx")
 
     # 删除节点
     print("---" * 10)
-    for child in root.findall("neighbor"):
-        root.remove(child)
-    xml_update_data = ET.tostring(root, encoding="unicode")
+    # for child in root.findall("neighbor"):
+    #     root.remove(child)
+    parse_xpath.remove_childnode("neighbor")
 
+    # print("---" * 10, root.tag)
+    xml_update_data = ET.tostring(root, encoding="unicode")
+    # print("xml_update_data", xml_update_data)
     # 操作后保存到xml_write_data.xml
     import codecs
     fp = codecs.open("xml_write_data.xml","w","utf-8")
